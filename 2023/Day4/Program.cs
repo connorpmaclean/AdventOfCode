dd7ec52cf1b7ac90ef6bad2329ebf77a48fcb586
Connor MacLean <connorpatrickmaclean@gmail.com>
2023-12-03 21:49:35 -0800

    Finished day4

---
+++ 2023/Day4/Program.cs
@@ -0,0 +1,63 @@
+﻿await Problem1();
+await Problem2();
+
+static async Task Problem1()
+{
+    var lines = await File.ReadAllLinesAsync("data2.aoc");
+
+    int score = 0;
+    foreach (var line in lines)
+    {
+        var comp = line.Split(':');
+        var cardSections = comp[1].Split("|");
+
+        var winningNumbers = new HashSet<string>(cardSections[0].Split(" ").Where(x => x != ""));
+        var cardNumbers = cardSections[1].Split(" ").Where(x => x != "");
+
+        int numMatch = cardNumbers.Where(n => winningNumbers.Contains(n)).Count();
+
+        score += (int)Math.Pow(2, numMatch - 1);
+    }
+
+    Console.WriteLine(score);
+}
+
+
+static async Task Problem2()
+{
+    var lines = await File.ReadAllLinesAsync("data2.aoc");
+
+    int score = 0;
+    Dictionary<int, int> scoreByCard = new();
+
+    Queue<(int, string)> queue = new();
+
+    for (int i = 0; i < lines.Length; i++ )
+    {
+        queue.Enqueue((i + 1, lines[i]));
+    }
+
+    while (queue.TryDequeue(out (int index, string line) nextItem))
+    {
+        if (!scoreByCard.TryGetValue(nextItem.index, out int cardScore))
+        {
+            var comp = nextItem.line.Split(':');
+            var cardSections = comp[1].Split("|");
+
+            var winningNumbers = new HashSet<string>(cardSections[0].Split(" ").Where(x => x != ""));
+            var cardNumbers = cardSections[1].Split(" ").Where(x => x != "");
+
+            cardScore = cardNumbers.Where(n => winningNumbers.Contains(n)).Count();
+            scoreByCard.Add(nextItem.index, cardScore);
+        }
+
+        for (int i = nextItem.index; i < nextItem.index + cardScore; i++)
+        {
+            queue.Enqueue((i + 1, lines[i]));
+        }
+
+        score++;
+    }
+
+    Console.WriteLine(score);
+}
