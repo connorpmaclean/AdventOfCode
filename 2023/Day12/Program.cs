69d2a83db5290a43eca6f1a22dc2c777b4abbf69
Connor MacLean <comaclea@microsoft.com>
2023-12-12 10:19:50 -0800

    Day12 completed

--- 2023/Day12/Program.cs
+++ 2023/Day12/Program.cs
@@ -1,7 +1,18 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 using System.Text;
 
-await Problem2();
+//var p1 = await Problem1();
+var p2 = await Problem2();
+
+//foreach (var item in p1)
+//{
+//    if (p2[item.Key] != item.Value)
+//    {
+//        Console.WriteLine($"{item.Key} {item.Value} {p2[item.Key]}");
+//    }
+//}
+
 
 static IEnumerable<IEnumerable<T>> GetKCombs<T>(IEnumerable<T> list, int length) where T : IComparable
 {
@@ -51,13 +62,15 @@ static IEnumerable<T[]> CombinationsRosettaWoRecursion<T>(T[] array, int m)
     }
 }
 
-static async Task Problem1()
+static async Task<Dictionary<string, long>> Problem1()
 {
+    var results = new Dictionary<string, long>();
     var lines = await File.ReadAllLinesAsync("./Data1.aoc");
 
     long count = 0;
     foreach (string line in lines)
     {
+        long localCount = 0;
         var comp = line.Split(' ');
         var map = comp[0];
         var groups = comp[1].Split(",").Select(int.Parse).ToArray();
@@ -67,7 +80,9 @@ static async Task Problem1()
 
         if (missingSprings == 0)
         {
-            count++;
+            localCount++;
+            results[line] = localCount;
+            count += localCount;
             continue;
         }
 
@@ -86,61 +101,84 @@ static async Task Problem1()
 
             if (Enumerable.SequenceEqual(groups, testGroups))
             {
-                count++;
+                localCount++;
             }
         }
 
+        results[line] = localCount;
+        count += localCount;
     }
 
     Console.WriteLine(count);
+    return results;
 }
 
 
-static async Task Problem2()
+static async Task<Dictionary<string, long>> Problem2()
 {
+    var results = new Dictionary<string, long>();
     var lines = await File.ReadAllLinesAsync("./Data1.aoc");
 
     long count = 0;
+    int row = 0;
     foreach (string line in lines)
     {
+        long localCount = 0;
+        if (line.StartsWith("//"))
+        {
+            continue;
+        }
+
+        Console.Write($"{row++}: {line} => ");
         var comp = line.Split(' ');
         var mapTemp = comp[0];
 
         string map = new StringBuilder()
             .Append(mapTemp)
-            //.Append(mapTemp)
-            //.Append(mapTemp)
-            //.Append(mapTemp)
-            //.Append(mapTemp)
+            .Append("?")
+            .Append(mapTemp)
+            .Append("?")
+            .Append(mapTemp)
+            .Append("?")
+            .Append(mapTemp)
+            .Append("?")
+            .Append(mapTemp)
             .ToString();
             ;
 
-        Console.WriteLine(map);
-
         var groupsTemp = comp[1].Split(",").Select(int.Parse).ToArray();
-        var groups = groupsTemp;
-            //.Concat(groupsTemp).Concat(groupsTemp).Concat(groupsTemp).Concat(groupsTemp).ToArray();
+        var groups = groupsTemp
+                .Concat(groupsTemp).Concat(groupsTemp).Concat(groupsTemp).Concat(groupsTemp)
+                .ToArray();
 
         int missingSprings = groups.Sum() - map.Count(c => c == '#');
 
         if (missingSprings == 0)
         {
-            count++;
-            continue;
+            localCount++;
         }
-
-        long localCount = GetPossibilities(new StringBuilder(map), groups, 0);
+        else
+        {
+            localCount = GetPossibilities(map.AsSpan(), groups, new Dictionary<Key, long>());
+        }
 
         count += localCount;
         Console.WriteLine(localCount);
+        results[line] = localCount;
     }
 
-    Console.WriteLine("Total: " + count);
+    Console.WriteLine(count);
+    return results;
 }
 
-static long GetPossibilities(StringBuilder map, Span<int> remaining, int mapStart)
+static long GetPossibilities(ReadOnlySpan<char> map, ReadOnlySpan<int> remaining, IDictionary<Key, long> cache)
 {
-    
+    Key key = new Key(map, remaining);
+    if (cache.TryGetValue(key, out long value))
+    {
+        return value;
+    }
+
     int current = remaining[0];
 
     int minSpacesRemaining = current - 1;
@@ -150,22 +188,23 @@ static long GetPossibilities(StringBuilder map, Span<int> remaining, int mapStar
     }
 
     long possibilities = 0;
-    for (int i = mapStart; i < map.Length - minSpacesRemaining; i++)
+    for (int i = 0; i < map.Length - minSpacesRemaining; i++)
     {
-        StringBuilder mapCopy = new StringBuilder(map.ToString());
+        //StringBuilder mapCopy = new StringBuilder(map.ToString());
 
         bool valid = true;
         int j = i;
+        char originalChar = map[i];
         for (; j < i + current; j++)
         {
-            if (mapCopy[j] == '.')
+            if (map[j] == '.')
             {
                 valid = false;
                 break;
             }
             else
             {
-                mapCopy[j] = '#';
+                //mapCopy[j] = '#';
             }
         }
 
@@ -173,17 +212,76 @@ static long GetPossibilities(StringBuilder map, Span<int> remaining, int mapStar
         {
             if (remaining.Length == 1)
             {
-                return 1;
+                // There should be no more springs in the map
+                if (map.Length == j)
+                {
+                    possibilities += 1;
+                }
+                else
+                {
+                    var mapAfter = map.Slice(j);
+                    bool hasNoSprings = true;
+                    for (int k = 0; k < mapAfter.Length; k++)
+                    {
+                        if (mapAfter[k] == '#')
+                        {
+                            hasNoSprings = false;
+                            break;
+                        }
+                    }
+
+                    if (hasNoSprings)
+                    {
+                        possibilities += 1;
+                    }
+                }
             }
-            else if (mapCopy[j] == '.' || mapCopy[j] == '?')
+            else if (map[j] == '.' || map[j] == '?')
             {
-                mapCopy[j] = '.';
-                possibilities += GetPossibilities(mapCopy, remaining.Slice(1), j + 1);
+                //mapCopy[j] = '.';
+                possibilities += GetPossibilities(map.Slice(j + 1), remaining.Slice(1), cache);
             }
         }
 
-        
+        if (originalChar == '#') // if we hit a spring, the group must start here, so no point going further
+        {
+            break;
+        }
     }
 
+    cache[key] = possibilities;
     return possibilities;
+}
+
+public record Key(string mapString, string remainingValue)
+{
+    public Key(ReadOnlySpan<char> map, ReadOnlySpan<int> remaining)
+        : this (map.ToString(), GetRemainingValue(remaining))
+    {
+    }
+
+    private static string GetRemainingValue(ReadOnlySpan<int> remaining)
+    {
+        var sb = new StringBuilder();
+        foreach (int i in remaining)
+        {
+            sb.Append(i);
+            sb.Append(',');
+        }
+
+        return sb.ToString();
+        //checked
+        //{
+        //    long val = 0;
+        //    long multiplier = 1;
+        //    foreach (var item in remaining)
+        //    {
+        //        val += item * multiplier;
+        //        multiplier *= 10;
+        //    }
+
+        //    return val;
+        //}
+        
+    }
 }
