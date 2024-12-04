e19b79ebe47fd8170e9e3fdbbdfacee826d716fc
Connor MacLean <connorpatrickmaclean@gmail.com>
2024-12-04 08:34:19 -0800

    Day 2 partly finished

---
+++ 2024/Day2/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AocHelper;
+
+Console.WriteLine("Hello, World!");
+
+
+var rows = File.ReadAllLines("./sample.aoc");
+
+int safeCount = 0;
+foreach (var row in rows)
+{
+    row.ParseMany<int>("", " ", out int[] values);
+
+    IList<int> data = values;
+    if (values[1] > values[0])
+    {
+        data = data.Reverse().ToList();
+    }
+
+
+    int safe = 2;
+    foreach (var it in data.GetDoubleIterator<int>())
+    {
+        int diff = it.Item1 - it.Item2;
+        if (diff >= 4 || diff <= 0)
+        {
+            if (--safe <= 0)
+            {
+                break;
+            }
+        }
+    }
+
+    safe = Math.Min(safe, 1);
+    safeCount += safe;
+}
+
+Console.WriteLine(safeCount);
---
+++ Helpers/AocHelper/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AocHelper
+{
+    public static class EnumerableExtensions
+    {
+
+        public static IEnumerable<(T, T)> GetDoubleIterator<T>(this IList<T> input)
+        {
+            for (int i = 0; i < input.Count - 1; i++) 
+            {
+                yield return (input[i], input[i + 1]);
+            }
+        }
+    }
+}
