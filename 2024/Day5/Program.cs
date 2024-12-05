1db167e95e4fcf70c4816d4419e1e4dd1accc9ca
Connor MacLean <connorpatrickmaclean@gmail.com>
2024-12-04 22:33:17 -0800

    Solved Day 5

--- 2024/Day5/Program.cs
+++ 2024/Day5/Program.cs
@@ -6,7 +6,7 @@ internal class Program
 
     private static void Main(string[] args)
     {
-        P1();
+        P2();
     }
 
     public static void P1()
@@ -93,9 +93,105 @@ internal class Program
 
     }
 
+    public static void P2()
+    {
+        var lines = File.ReadAllLines("data.aoc");
+
+        Dictionary<int, Rule> ruleDict = new Dictionary<int, Rule>();
+
+        int i = 0;
+        for (; i < lines.Length; i++)
+        {
+            string? line = lines[i];
+            if (line == string.Empty)
+            {
+                break;
+            }
+
+            line.ParseMany("", "|", out int[] values);
+
+            var first = ruleDict.GetOrAdd(values[0], () => new Rule(values[0]));
+            first.MustBeBefore.Add(values[1]);
+
+            var second = ruleDict.GetOrAdd(values[1], () => new Rule(values[1]));
+            second.MustBeAfter.Add(values[0]);
+        }
+
+        i++;
+
+        long result = 0;
+        for (; i < lines.Length; i++)
+        {
+            string? line = lines[i];
+            line.ParseMany("", ",", out int[] values);
+
+
+            bool failed = false;
+            var disallowed = new HashSet<int>();
+            foreach (var value in values)
+            {
+                if (disallowed.Contains(value))
+                {
+                    failed = true;
+                    break;
+                }
+
+                if (ruleDict.TryGetValue(value, out Rule rule))
+                {
+                    foreach (var addToDisallow in rule.MustBeAfter)
+                    {
+                        disallowed.Add(addToDisallow);
+                    }
+                }
+            }
+
+            disallowed = new HashSet<int>();
+            foreach (var value in values.Reverse())
+            {
+                if (disallowed.Contains(value))
+                {
+                    failed = true;
+                    break;
+                }
+
+                if (ruleDict.TryGetValue(value, out Rule rule))
+                {
+                    foreach (var addToDisallow in rule.MustBeBefore)
+                    {
+                        disallowed.Add(addToDisallow);
+                    }
+                }
+            }
+
+            
+            if (failed)
+            {
+                var ruleList = new List<Rule>();
+
+                foreach (int value in values)
+                {
+                    var rule = ruleDict[value];
+                    rule.SortValue = rule.MustBeAfter.Count(x => values.Contains(x));
+                    ruleList.Add(rule);
+                }
+
+                var ordered = ruleList.OrderBy(x => x.SortValue).ToList();
+                result += ordered[ordered.Count / 2].id;
+                
+            }
+
+        }
+
+        Console.WriteLine(result);
+
+
+    }
+
     public record Rule(int id)
     {
         public HashSet<int> MustBeBefore = new();
         public HashSet<int> MustBeAfter = new();
+
+        public int SortValue { get; set; } = 0;
     }
 }
